2043889a74511a3a98ffb1029470695c442b6ea6
naelsondouglas <naelson17@gmail.com>
2023-01-17 02:02:03 -0300

    fixed the misplace_precondition

--- testcases/dependency_rules/misplaced_precondition.py
+++ testcases/dependency_rules/misplaced_precondition.py
@@ -6,6 +6,10 @@ ensure_like = expand_words(('ensure', 'check'), k=15)
 
 patterns = [
             [
-                {'RIGHT_ID': 'anchor','RIGHT_ATTRS': {'POS': {'IN': ['VERB', 'VB'], 'LOWER': {'IN': ensure_like}}, 'IS_SENT_START':True}}
+                {'RIGHT_ID': 'anchor','RIGHT_ATTRS': {'POS': {'IN': ['VERB', 'VB']}, 'IS_SENT_START':True}},
+                {'RIGHT_ID': 'side','LEFT_ID':'anchor', 'REL_OP':'>', 'RIGHT_ATTRS': {'DEP': 'dobj', 'OP':'!'}}
             ]
         ]
+
+#"RIGHT_ATTRS": {"DEP": "nsubj"}
+#4 - Misplaced Pre-Condition
