05ba8b3fd36a62fb630dece48d158c107c8e5cff
mantertius <31227589+mantertius@users.noreply.github.com>
2023-01-22 00:56:29 -0300

    [feat] adds splitting by 'N.'

--- testcases/moto_regex.py
+++ testcases/moto_regex.py
@@ -22,6 +22,8 @@ def moto_smell_loader_closure():
     df = df[[NAME_COL, PRECON_COL, STEPS_COL, RESULTS_COL, SMELL_COL]]
     print(df)
     def smells_loader(smell_acronym: str) -> pd.DataFrame:
+        if smell_acronym == '':
+            return df.reset_index(drop=True)
         return df.loc[df[SMELL_COL].apply(lambda x: smell_acronym in x)].reset_index(drop=True)
     return smells_loader
 
@@ -45,13 +47,18 @@ def extract_texts(text:str) -> abc.Container:
     #print(text)
     spaces = r'\s{2,}'
     breaks = r'\n+'
+    before_breaks = r'\s+\n+'
     steps_number = r'\*\s*(step|Step)\s*\d+\*\s*\n*'
     null = r'\*\s*(step|Step)\s*\d+\* null\n{0,}?'
     text = re.sub(re.compile(null), '', text)
     #print(text)
-    text = re.split(re.compile(steps_number), text) 
-    #breakpoint()
+    text = re.split(re.compile(steps_number), text)
     stripped_text = [token.strip() for token in text if token and not token.isspace() and token != 'Step']
+    print(stripped_text)
+    #breakpoint()
+    if stripped_text and len(stripped_text) == 1:
+        if re.findall(r'\d+\.', stripped_text[0]): #type(stripped_text)== list and 
+            stripped_text = re.split(r'\d+\. ',stripped_text[0])
     #print(f'This is the new text: {stripped_text}')
 
     # print(new_text)
@@ -59,7 +66,8 @@ def extract_texts(text:str) -> abc.Container:
     for token in stripped_text:
         token = re.sub(re.compile(breaks), '', token)
         token = re.sub(re.compile(spaces),' ', token)
-        final_text.append(token)
+        if token != '':
+            final_text.append(token.strip())
     # text = re.sub(re.compile(breaks), '', text)
     # text = re.sub(re.compile(spaces),' ', text)
     print('-----------------------------')
@@ -99,15 +107,24 @@ def get_tests(smell_acronym : str):
         # breakpoint()
     return result
 
+def get_tests_from_name(test_name:str):
+    df = smells_loader('')
+    test_row = df.loc[df[NAME_COL].apply(lambda x: test_name in x)]
+    result = []
+    for row in test_row.itertuples(name = 'Teste', index=False):
+        name = row[0]
+        print(name)
+        header = pipeline(row[1])
+        actions = pipeline(row[2])
+        reactions = pipeline(row[3])
+        temp = [Test(name= name, header=header, steps=Step(actions, reactions))]
+        result.append(temp)
+        # breakpoint()
+    return result
+
 if __name__ == '__main__':
     df = moto_smell_loader_closure()
-#     text = '''*Step 1*
-#   1. Test on projects that support Wireless Charging (Reverse Charging)
-#   2. First time that Power Sharing is being used on DUT
-#   3. Mobile phone power is at least 20%
-#   4. Power sharing will be automatically disabled after 1 minute if isn't detect a device
-# *Step 2* null
-# *Step 2* null'''
-#     extract_texts(text)#moto_ambt()
-    result = get_tests('AmbT')
+    result = get_tests('None')
     print(result)
+    # rs = get_tests_from_name('TC - [Folio]-Turn on/off Folio when the screen is lock/Unlock')
+    # print(rs)
