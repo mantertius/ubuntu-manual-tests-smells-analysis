b8a903081123c03570f03859998d608ad202d331
mantertius <mpat@ic.ufal.br>
2023-02-02 17:22:37 -0300

    refactors nlp call

--- testcases/pipeline.py
+++ testcases/pipeline.py
@@ -3,7 +3,10 @@ from collections import namedtuple
 
 #TODO: Turn it into a singleton
 nlp = spacy.load('en_core_web_lg')
-nlp_pt = spacy.load('pt_core_news_lg')
+try:
+    nlp_pt = spacy.load('pt_core_news_lg')
+except OSError:
+    print('Instale o modelo:\npython -m spacy download pt_core_news_lg')  
 
 Test = namedtuple('Test',['file','header','steps'])
 Step = namedtuple('Step', ['action', 'reactions']) #Step([action],[reactions])
