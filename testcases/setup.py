f807e45a1d2334dc0ed2eacf960c0f4c082b393d
Manoel Terceiro <mpat@ic.ufal.br>
2023-02-08 15:58:34 -0300

    Update setup.py

--- testcases/setup.py
+++ testcases/setup.py
@@ -9,9 +9,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='manual_test_smells',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
-    install_requires=['spacy',
+    install_requires=['spacy==3.5',
                         'pandas',
                         'rich',
                         'scipy',
