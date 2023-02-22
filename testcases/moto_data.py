9d573853dfeb5a0dba515284d9584e004f91f4a2
eas5 <eas5@cin.ufpe.br>
2023-02-22 17:22:42 -0300

    Improved moto data parsing

--- testcases/moto_data.py
+++ testcases/moto_data.py
@@ -50,7 +50,7 @@ def normalize_text(text: str) -> str:
 
 
 def pipeline_steps(steps_raw_text: str) -> dict:
-    regex = re.compile(r'(\*.+\*)')
+    regex = re.compile(r'(\*Step\s\d+\*)')
     splits = [split for split in regex.split(steps_raw_text) if split]
     step_numbers = [int(re.compile(r'\d+').findall(key)[0]) for key in splits[0:-1:2]]
     steps = [step for step in splits[1::2]]
@@ -72,25 +72,18 @@ def get_tests(smell_acronym: str):
     df = smells_loader(smell_acronym)
     result = []
     for row in df.itertuples(name='Teste', index=False):
-        test_have_substeps = has_substeps(row[2]) and has_substeps(row[3])
-        if not test_have_substeps:
-            name = row[0]
-            header = ''  # not actually in use, would be row[2]
-            actions = pipeline_steps(row[2])
-            reactions = pipeline_steps(row[3])
-            steps = list()
-            for step, description in actions.items():
-                if step in reactions and reactions[step] != '':
-                    step = Step(nlp(description), [nlp(reactions[step])])
-                else:
-                    step = Step(nlp(description), [])
-                steps.append(step)
-            test = Test(file=name, header=header, steps=steps)
-            result.append([test])
+        name = row[0]
+        header = ''  # not actually in use, would be row[2]
+        actions = pipeline_steps(row[2])
+        reactions = pipeline_steps(row[3])
+        steps = list()
+        for step, description in actions.items():
+            if step in reactions and reactions[step] != '':
+                step = Step(nlp(description), [nlp(reactions[step])])
+            else:
+                step = Step(nlp(description), [])
+            steps.append(step)
+        test = Test(file=name, header=header, steps=steps)
+        result.append([test])
     log.info('End of Moto retrieving.')
     return result
-
-
-def has_substeps(action_or_reaction: str) -> bool:
-    substep_marker = re.compile('\n\W*\d+\.\s*\w+')
-    return len(substep_marker.findall(action_or_reaction)) > 0
